a9e05ccd011ca20ce52b003b8759a3d10f999364
EmanuelCChagas <82842431+EmanuelCChagas@users.noreply.github.com>
2021-05-04 14:40:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,105 +1,135 @@
- /******************************************************************************
+#include <stdio.h> //biblioteca padrão do C (comandos como printf, scanf, entre outros)
+#include <stdlib.h> // alocação de memoria
+#include <locale.h> // mudança de linguagem para português (para aceitar caracteres como ç)
+#include <math.h> // instruções matemáticas
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <math.h>
-
-int numero_escolhido;
-int num_1 = 0; // opcao de so     
-int num_2 = 0; // opcao de so
-int num_3 = 0; // opcao de so  
-int num_4 = 0; // opcao de so  
-int num_5 = 0; // opcao de so  
-int num_6 = 0; // opcao de so  
-
-int por_1; // porcentagem
-int por_2; // porcentagem
-int por_3; // porcentagem
-int por_4; // porcentagem
-int por_5; // porcentagem
-int por_6; // porcentagem
-int total;
 int main(int argc,char *argv[])
 {
-  do { 
-      
-      printf("Qual o melhor sistema operacional para uso em servidores?\n");
-        printf("-------------------------------------------------------\n");
-        printf("1- Windows Server\n");
-        printf("2 - Unix\n");
-        printf("3 - Linux\n");
-        printf("4 - Netware\n");
-        printf("5 - Mac OS\n");
-         printf("6 - Outro\n\n");
-        printf(" Para ver os resultados digite 0\n");
-        scanf("%d" , &numero_escolhido);
-         if(numero_escolhido > 6)  // nao aceitar valor maior que 6
-         {
-             printf("numero invalido");
-             return 0;
-         };
-         
-        if (numero_escolhido <0){  // nao aceitar valor menor que 0
-            printf("numero invalido");
-             return 0;
-        
-        };
-         switch (numero_escolhido) // escolha de votacao
-                       {
-                           case 1:
-                                printf("\nvoto computado\n\n"); //mostrar ao usuario que seu voto foi computado
-                                 num_1=num_1 + 1; //adicionar voto
-                                 break; //voltar ao menu
-                            case  2:
-                                 printf("\nvoto computado\n\n");
-                                    num_2=num_2 + 1;
-                                     break;
-                          
-                           case  3: 
-                            printf("\nvoto computado\n\n");
-                              num_3=num_3 + 1;
-                                     break;
-                          
-                           case  4:
-                           printf("\nvoto computado\n\n");
-                              num_4=num_4 + 1;
-                                     break;
-                           
-                           case  5:
-                            printf("\nvoto computado\n\n");
-                              num_5=num_5 + 1;
-                                     break;
-                           case  6:
-                            printf("\n computado\n\n");
-                              num_6=num_6 + 1;
-                              break;
-                        
-                                            }
+    setlocale(LC_ALL,"Portuguese"); // mudança de linguagem para português
 
-                  }
-    while(numero_escolhido != 0); //enquanto o numero nao for 0 fazer o loop
-    
-    total= (num_1 + num_2 + num_3 + num_4 + num_5 + num_6); //soma do total
-    por_1= (num_1*100)/(total);// regra de 3 para cada um
-    por_2= (num_2*100)/(total);// regra de 3 para cada um
-    por_3= (num_3*100)/(total);// regra de 3 para cada um
-    por_4= (num_4*100)/(total);// regra de 3 para cada um
-    por_5= (num_5*100)/(total);// regra de 3 para cada um
-    por_6= (num_6*100)/(total);// regra de 3 para cada um
-    printf("\n Resultado: \n\n ");
-    printf("\n total de votos %d\n\n", total);
-    printf("1- Windows Server= %d  votos e %d %\n", num_1,por_1);
-    printf("2-  Unix= %d votos e %d %\n", num_2,por_2);
-    printf("3 - Linux= %d votos e %d %\n", num_3,por_3);
-    printf("4 - Netware= %d votos e %d %\n",num_4,por_4);
-    printf("5 - Mac OS= %d votos e %d %\n", num_5,por_5);
-    printf("6 - Outro= %d votos e %d %\n", num_6,por_6);
-    
-    return 0;
-}
+    //variaveis globais
+
+    int opcao,resp1,resp2;
+    int numdec, rem=0, decnumbin = 0, i=1;
+    int binnum, rem1,bindec=0, i1=0;
+
+    // inicio da calculadora
+
+    printf("CALCULADORA DE CONVERSÃO DE NÚMEROS BINARIOS\n");
+    printf("------------------------------------------\n");
+    printf("Você deseja converter de Decimal para Binario ou de Binario para Decimal?\n");
+    printf("\n");
+    printf("1-Decimal para Binario\n");
+    printf("2-Binario para Decimal\n\n\n");
+    scanf("%d", &opcao);
+
+     switch(opcao){  //responsável pelas instruções das opções de conversão
+     case 1:          // primeiro caso, decimal para binário
+          do{
+                rem=0; //zerar o valor das variaveis para o loop ser feito
+                numdec=0;
+                i=1;
+                decnumbin=0;
+
+                printf("\n Digite o numero decimal de 1 a 999:\n");
+                scanf("%d", &numdec);
+                if(numdec>999){ //valor máximo que possa ser convertido
+                    printf("Valor decimal invalido\n\n\n");
+                        break;
+                              };
+
+                while(numdec)  // loop para a conversão do decimal para o número binario
+                {
+                   rem = numdec % 2;
+                   numdec = numdec / 2;
+                   decnumbin =  decnumbin + (rem * i) ;
+                   i = i * 10;
+                }
+                /* loop funciona assim
+                exemplo:
+                10=número decimal-> 10=numdec
+                1º conta===10/2=5 restante=0 ->
+                                                   numdec=10 rem=0 numdec=5 decnumbin= 0 + (0*1)=0
+
+                2º conta===5/2=2 restante=1  ->
+                                                  numdec=5  rem=1 numdec=2 decnumbin= 0 + (1*10)=10
+
+                3º conta===2/2=1 restante=0 ->
+                                                 numdec=2 rem=0 decnumbin= 10+(0*100)=10
+
+                4º conta===1/2 restante=1   -> numdec=1 rem=1 decnumbin= 10+(1*1000)= 1010
+
+                resultado=1010
+                */
+               system("cls"); //limpar log
+               printf("O resultado é %11d\n",decnumbin); //mostrar resultado
+
+               printf("Deseja fazer outra conversão?\n"); //decisão do usuário de fazer outra conversão
+               printf("1-Fazer outra conversão\n");
+               printf("2-Sair\n");
+               scanf("%d", &resp2);
+               if(resp2!=1){ //sair caso não queira fazer outra conversão
+                       break;
+                           };
+             }
+           while(resp2=1); //continuar fazendo o codigo enquanto for escolhido fazer outra conversão
+           break;
+
+     case 2: //segundo caso binário para decimal
+        do{
+            binnum=0; //zerar o valor das variaveis para o loop ser feito
+            rem1=0;
+            i1=0;
+            bindec=0;
+            printf("\n Digite o numero binário sem espaçamento que tenha o valor menor que 999 em decimal:\n");
+            scanf("%d", &binnum);
+            if(binnum>=1111100111){ //valor máximo que possa ser convertido
+                printf("Número binário invalido\n\n\n");
+                        break;}
+
+
+
+             while(binnum > 0) { //loop para número decimal
+            rem1= binnum % 10;
+            bindec=bindec + rem1 * pow(2,i1);
+            binnum=binnum/10;
+            i1++;
+                              }
+            /*   loop funciona assim
+                exemplo:
+
+              1010: número binário=binnum
+            1º conta:===== binnum=1010/10=101 restante=0 ->
+                                                    binnum=1010 rem=1 bindec=0 + 0 * pow(2,0) -> bindec=0+0*1=0
+            2º conta====== binnum=101/10=10 restante=1 ->
+                                                   binnum=101 rem=1  bindec= 0 + 1 *pow(2,1) -> bindec=0+1*2=2
+            3º conta=======binnum=10/10=1 restante=0  ->
+                                                   binnum=10 rem=0 bindec=2+0*pow(2,2) -> bindec=2+0*4=2
+            4º conta=======binnum=1/10=0 restante=1  ->
+                                                   binnum=1 rem=1 bindec=2+1*pow(2,3) ->=bindec=2+1*8=10
+             */
+           system("cls"); //limpar log
+           printf("O resultado é %d\n",bindec);//mostrar resultado
+           printf("Deseja fazer outra conversão?\n"); //decisão do usuário de fazer outra conversão
+           printf("1-Fazer outra conversão\n");
+           printf("2-Sair\n");
+           scanf("%d", &resp1);
+           if(resp1!=1){ //sair caso não queira fazer outra conversão
+            break;
+           };
+               }
+           while(resp1=1); //continuar fazendo o codigo enquanto for escolhido fazer outra conversão
+           break;
+
+
+
+     default: //caso nem uma opção definida for escolhida
+         printf("Opção invalida \n\n"); //escrever opção invalida
+
+         break; //sair
+                }
+
+     system("pause"); //executa o comando pause do sistema operacional que fará o console ficar esperando por uma tecla
+     return 0;
+
+  }
